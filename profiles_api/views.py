8bbec02b7f36500ad237b4eefb7b6a8c5f7007dc
princeverma03 <princeverma1711@gmail.com>
2021-03-22 20:38:35 +0530

    Added GET,PUT,POST,PATCH and DELETE methods to our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+
+class HelloApiView(APIView):
+    '''
+    Test APIview
+    '''
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        '''
+        Returns a list of API view features
+        '''
+        an_apiview = [
+            'Uses HTTP method as functions (get, post, put, patch, delete)',
+            'Is similar to a Django View',
+            'Gives you the most control over the application logic',
+            'Is mapped manually to URLs'
+        ]
+
+        return Response({'message':'Hello!', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        '''
+        Create a hello mesage withour name
+        '''
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello ! {name}'
+            return Response({'message': message})
+
+        else:
+            return Response(serializer.errors,
+                            status = status.HTTP_400_BAD_REQUEST
+                            )
+    def put(self, request, pk=None):
+        '''
+        Handle updating entire object (replace operation)
+        '''
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        '''
+        Handle partial update of an object (modify operation)
+        '''
+        return Response({'method' : 'PATCH'})
+
+    def delete(self, request, pk=None):
+        '''
+        Delete an Object from DB
+        '''
+        return Response({'method': 'DELETE'})
